642d4ee753382fdda37b7fa232fef307e5da6222
Syed Muzamil <smmhd121@gmail.com>
2020-04-28 15:31:27 +0530

    Added some dependencies

--- threline/settings.py
+++ threline/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,11 +77,11 @@ WSGI_APPLICATION = 'threline.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'threline_db',
-        'USER': 'threlinedb',
-        'PASSWORD': 'threlinedb',
+        'NAME': 'postgress',
+        'USER': 'postgress',
+        'PASSWORD': 'sMuzi@3492#',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
     }
 }
 
@@ -121,4 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
